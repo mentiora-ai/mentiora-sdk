0d1daf364d0c0ea0b1e67e8e72fbb2d6e6bc1aba
ThomasPeriscoop <thomasda@periscoop.com>
2026-01-22 19:30:51 +0100

    chore: update Docusaurus configuration for GitHub Pages deployment and remove old CNAME

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.mentiora.ai',
+  url: 'https://periscoop.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/mentiora-sdk/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
